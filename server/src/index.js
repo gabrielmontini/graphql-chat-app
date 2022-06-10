d3d7af48bbcf58d8acf9773a8167d4638e782d5b
gabrielmontini <gabriel.montini.silva@gmail.com>
2022-06-10 16:55:48 -0300

    Server side done and started vue front

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const { createServer, createPubSub } = require('graphql-yoga')
+const typeDefs = require('./schema')
+const resolvers = require('./resolver')
+
+const pubsub = createPubSub();
+const server = new createServer({typeDefs, resolvers, context: { pubsub }})
+
+server.start(() => console.log('Server running on localhost:4000'))
+
+// import { GraphQLServer } from 'graphql-yoga'
+
+// const typeDefs = ``; // Our definitions need to be in template literals
+
+// const resolvers = {};
+
+// const server = new GraphQLServer({ typeDefs, resolvers }) ;
+
+// server.start(() => console.log('server running'));
